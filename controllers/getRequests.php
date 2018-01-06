8193c593951dd1b1a5bd91c0cea8a852ab0b576c
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2018-01-06 19:28:25 +0100

    View med getkald mangler twig repræsentation. Men kaldes som objekt fra xml soapenvelop fra wsdl kontrakten til webservice.

--- controllers/getRequests.php
+++ controllers/getRequests.php
@@ -11,13 +11,15 @@ Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('../views');
 $twig = new Twig_Environment($loader, array('auto_reload' => true));
-$template = $twig->loadTemplate('indexBox.html.twig');
+
 
 $wsdl = "http://boxvolumecaltrialexameasw20180106012921.azurewebsites.net/BoxCalService.svc?wsdl";
 $client1 = new SoapClient($wsdl);
 $result1 = $client1->GetAllRequest();
 
+
 print_r($result1);
 
+$template = $twig->loadTemplate('allRequestsView.html.twig');
 $parametersToTwig = array("requests"=>$result1);
 echo $template->render($parametersToTwig);
